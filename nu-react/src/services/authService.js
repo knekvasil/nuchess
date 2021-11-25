bc66c2a851a40771d289ada0d0e03c51eb3f1214
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-11-25 10:51:42 -0500

    created auth service

---
+++ nu-react/src/services/authService.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+const tokenString = "jwtcoffeegallery";
+
+// POST/Login
+export const loginUserToApi = async (user) => {
+	const response = await axios.post(`${apiUrl}/auth/login`, user);
+	try {
+		console.log(response.data);
+		if (response.data) {
+			const { _id, role, name } = response.data;
+			localStorage.setItem(
+				tokenString,
+				JSON.stringify({ id: _id, role, name })
+			);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+	return response;
+};
+
+// POST/Signup
+export const signUpUser = async (user) => {
+	const response = await axios.post(`${apiUrl}/auth/signup`, user);
+	return response;
+};
+
+// isAuth()
+export const isAuthenticated = () => {
+	if (typeof window == "undefined") {
+		return false;
+	}
+	if (localStorage.getItem(tokenString)) {
+		return JSON.parse(localStorage.getItem(tokenString));
+	}
+	return false;
+};
+
+export const logOut = () => {
+	localStorage.removeItem(tokenString);
+	window.location.reload();
+};
