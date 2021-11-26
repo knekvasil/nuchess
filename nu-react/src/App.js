865841a9ebf440ddfc9103c62e22917d634b480d
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-11-26 16:22:34 -0500

    added auth/admin routes

--- nu-react/src/App.js
+++ nu-react/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import HomeView from "./views/HomeView";
-
 import { Routes, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
+import HomeView from "./views/HomeView";
+import SignupView from "./views/SignupView";
+import LoginView from "./views/LoginView";
 
 function App() {
 	return (
@@ -10,6 +12,8 @@ function App() {
 			<NavBar />
 			<Routes>
 				<Route path="/" element={<HomeView />} />
+				<Route path="/signup" element={<SignupView />} />
+				<Route path="/login" element={<LoginView />} />
 			</Routes>
 		</div>
 	);
