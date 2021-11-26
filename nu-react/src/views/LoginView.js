d2ae0aa6965c80b38683feab768b7fc40e08569b
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-11-26 14:46:43 -0500

    login + signupviews functionality

--- nu-react/src/views/LoginView.js
+++ nu-react/src/views/LoginView.js
@@ -1,7 +1,57 @@
 // LoginView.js
 
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { loginUserToApi } from "../services/authService";
+
+import "./Form.css";
+
 function LoginView() {
-	return <p>LoginView</p>;
+	const navigate = useNavigate();
+	const [user, setUser] = useState({ email: "", password: "" });
+
+	function handleChange(event) {
+		setUser({ ...user, [event.target.name]: event.target.value });
+	}
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		try {
+			const userResponse = await loginUserToApi(user);
+			navigate("/");
+			await window.location.reload();
+		} catch (error) {
+			console.log(error);
+		}
+		setUser({ email: "", password: "" });
+	}
+
+	return (
+		<div className="container mt-5">
+			<form className="form">
+				<h2>Login View</h2>
+				<input
+					onChange={handleChange}
+					name="email"
+					value={user.email}
+					className="form-control"
+					type="text"
+					placeholder="email"
+				/>
+				<input
+					onChange={handleChange}
+					name="password"
+					value={user.password}
+					className="form-control"
+					type="password"
+					placeholder="password"
+				/>
+				<button onClick={handleSubmit} className="form-control btn btn-success">
+					Login
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default LoginView;
--- nu-react/src/views/SignupView.js
+++ nu-react/src/views/SignupView.js
@@ -1,7 +1,58 @@
 // SignupView.js
 
+import { useState } from "react";
+import { signUpUser } from "../services/authService";
+
+import "./Form.css";
+
 function SignupView() {
-	return <p>SignupView</p>;
+	const [user, setUser] = useState({ name: "", email: "", password: "" });
+
+	async function handleChange(event) {
+		setUser({ ...user, [event.target.name]: event.target.value });
+	}
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		signUpUser(user);
+		setUser({ name: "", email: "", password: "" });
+		alert("User Created");
+	}
+
+	return (
+		<div>
+			<form className="form">
+				<h2>Sign Up</h2>
+				<input
+					value={user.name}
+					name="name"
+					onChange={handleChange}
+					placeholder="name"
+					type="text"
+					className="form-control"
+				/>
+				<input
+					value={user.email}
+					onChange={handleChange}
+					name="email"
+					placeholder="email"
+					type="text"
+					className="form-control"
+				/>
+				<input
+					value={user.password}
+					onChange={handleChange}
+					name="password"
+					placeholder="password"
+					type="password"
+					className="form-control"
+				/>
+				<button onClick={handleSubmit} className="form-control btn btn-success">
+					Sign Up
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default SignupView;
