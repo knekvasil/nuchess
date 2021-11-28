81b4b4decfca54c1604129776ce7133b8454af05
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-11-28 12:07:08 -0500

    GalleryView pausing point

--- nu-react/src/views/GalleryView.js
+++ nu-react/src/views/GalleryView.js
@@ -1,6 +1,37 @@
 // GalleryView.js
 
+import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import PostCard from "../components/PostCard";
+import { getAllPosts } from "../services/postService";
+
 function GalleryView() {
+	const [posts, setPosts] = useState([]);
+	const [search, setSearch] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		getPost();
+	}, []);
+
+	async function getPost() {
+		setLoading(true);
+		setTimeout(async () => {
+			const response = await getAllPosts();
+			setPosts(response.data);
+			setLoading(false);
+		}, 2000);
+	}
+
+	function handleSearch(event) {
+		setSearch(event.target.value.toLowerCase);
+	}
+
+	function resetPosts() {
+		getPost();
+		setSearch("");
+	}
+
 	return <p>GalleryView</p>;
 }
 
